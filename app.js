f8c67bec7804fcd57f3cda745446ea9aa6dfc7ca
Rade Bajic <bajic.rade2@gmail.com>
2019-03-21 20:36:44 +0100

    notes

--- app.js
+++ app.js
@@ -1,22 +1,21 @@
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', function() {
+    navigator.serviceWorker.register('sw.js').then(function(registration) {
+      // Registration was successful
+      console.log('ServiceWorker registration successful with scope: ', registration.scope);
+    }, function(err) {
+      // registration failed :(
+      console.log('ServiceWorker registration failed: ', err);
+    });
+  });
+}
+
+// /////////////////////////////////////////////////////////////////////
+
 const contentDivs = document.querySelectorAll('div.sadrzina div');
 const button = document.querySelector('div.ispocetka button');
 let times = 0;
 let isAgain = false;
-
-// ///// REGISTROVACU GA OVDE
-
-if ('serviceWorker' in navigator) {
-    window.addEventListener('load', function() {
-      navigator.serviceWorker.register('/sw.js').then(function(registration) {
-        // Registration was successful
-        console.log('ServiceWorker registration successful with scope: ', registration.scope);
-      }, function(err) {
-        // registration failed :(
-        console.log('ServiceWorker registration failed: ', err);
-      });
-    });
-  }
-// //////////////
 
 const onMdHandler = function(ev){
 
